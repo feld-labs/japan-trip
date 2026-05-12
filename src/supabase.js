40a4265a6f3cce3e50cba4bdc2188b913ebdb07c
Brian Feldman <brian@feldand.co>
2026-05-11 22:55:28 -0400

    Update Supabase key + add Kyoto beer spots

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://uynonzgxhcyfxtymrzmz.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_zNPOT4_jhkPXozipd4NGNA_YQLGFtqQ";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV5bm9uemd4aGN5Znh0eW1yem16Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzNTA3MjIsImV4cCI6MjA5MzkyNjcyMn0.3PvWqO2_KagMECQe3X8dcy-_JxdL5uun_cXdyBJFlOI";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
